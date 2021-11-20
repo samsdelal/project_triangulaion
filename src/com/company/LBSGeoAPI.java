9406127778cb2be5e641e60685a1b87c10335dd7
Boris Kuznecov <oralebot@mail.ru>
2021-11-20 13:45:12 +0700

    print fix

--- src/com/company/LBSGeoAPI.java
+++ src/com/company/LBSGeoAPI.java
@@ -1,22 +1,19 @@
-package com.company;
+package src.com.company;
 
 import java.io.FileNotFoundException;
 import java.sql.SQLException;
+import java.util.Formatter;
 import java.util.HashMap;
 
 public class LBSGeoAPI {
 
     public void getCoordinates() throws FileNotFoundException, SQLException {
         ReadJSON nes = new ReadJSON();
-        //System.out.println(nes.getJson());
         HashMap<Integer, HashMap<Integer, HashMap<String, Integer>>> jj =  nes.getJson_1();
         HashMap<Integer, HashMap<Integer, HashMap<String, Integer>>> jj_2 =  nes.getJson_2();
 
-        //System.out.println(jj);
         ReadDataBase get = new ReadDataBase();
-//        get.connectionToDatabase(45005, 10795);
         for(int i: jj.keySet()){
-            //System.out.println(jj.get(i));
             double lon = 0;
             double lat = 0;
             double dbm = 0;
@@ -33,7 +30,6 @@ public class LBSGeoAPI {
                 int cellID = jj.get(i).get(gg).get("cellid");
                 int LAC = jj.get(i).get(gg).get("lac");
                 HashMap<String, Double> connect = get.connectionToDatabase(LAC, cellID);
-                //System.out.println(connect);
                 int powerInDdmb = jj.get(i).get(gg).get("power");
                 double powerCof = powerInDdmb/dbm;
                 lon += connect.get("lon") * powerCof;
@@ -46,50 +42,50 @@ public class LBSGeoAPI {
             HashMap<String, Double> send_final = new HashMap<>();
             send_final.put("lon", lonitude);
             send_final.put("lat", latitude);
-//            System.out.println(send_final);
             String result_lon = String.format("%.7f", lonitude);
             String result_lat = String.format("%.7f", latitude);
-            System.out.printf("Широта - %s, Долгота - %s +- 10 метров\n", result_lat, result_lon);
+            System.out.printf("╔ Широта - %s\n╠ Долгота - %s\n╠ Погрешность +- 10 метров\n╚ Ссылка для просмотра - %s\n", result_lat, result_lon,  getLink(result_lat, result_lon));
 
 
         }
         for(int i: jj_2.keySet()){
-            //System.out.println(jj_2.get(i));
             double lon = 0;
             double lat = 0;
             double dbm = 0;
             for(int powerDbm: (jj_2.get(i)).keySet()){
 
                 double powerInDdmb = jj_2.get(i).get(powerDbm).get("power");
-                //System.out.println(powerInDdmb);
                 dbm += powerInDdmb;
 
             }
-            //System.out.println(dbm);
 
             for(int gg: (jj_2.get(i)).keySet()){
                 int cellID = jj_2.get(i).get(gg).get("cellid");
                 int LAC = jj_2.get(i).get(gg).get("lac");
                 HashMap<String, Double> connect = get.connectionToDatabase(LAC, cellID);
-                //System.out.println(connect);
                 int powerInDdmb = jj_2.get(i).get(gg).get("power");
                 double powerCof = powerInDdmb/dbm;
                 lon += connect.get("lon") * powerCof;
                 lat += connect.get("lat") * powerCof;
             }
 
-            int countCells = jj_2.get(i).size();
             double lonitude = lon;
             double latitude = lat;
             HashMap<String, Double> send_final = new HashMap<>();
             send_final.put("lon", lonitude);
             send_final.put("lat", latitude);
-//            System.out.println(send_final);
             String result_lon = String.format("%.7f", lonitude);
             String result_lat = String.format("%.7f", latitude);
-            System.out.printf("Широта - %s, Долгота - %s +- 300 метров\n", result_lat, result_lon);
+            System.out.printf("╔ Широта - %s\n╠ Долгота - %s\n╠ Погрешность +- 300 метров\n╚ Ссылка для просмотра - %s\n", result_lat, result_lon,  getLink(result_lat, result_lon));
         }
 
+    }
+    private static Formatter getLink(String Lac, String Lat){
+        String lac = Lac.replace(",", ".");
+        String lat = Lat.replace(",", ".");
+        Formatter format = new Formatter();
+        format.format("https://geotree.ru/?lat=%s&lon=%s&z=16&mlat=%s&mlon=%s", lac, lat, lac, lat);
+        return format;
     }
 
 }
